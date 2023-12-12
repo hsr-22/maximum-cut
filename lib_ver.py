cc36340017fd541f0777e34190569912d30653f8
Harsh Roniyar <112925148+hsr-22@users.noreply.github.com>
2023-12-12 15:47:13 +0000

    First simulation

---
+++ lib_ver.py
@@ -0,0 +1,7 @@
+import numpy
+import networkx
+import matplotlib
+
+print("NumPy version:", numpy.__version__)
+print("NetworkX version:", networkx.__version__)
+print("Matplotlib version:", matplotlib.__version__)
